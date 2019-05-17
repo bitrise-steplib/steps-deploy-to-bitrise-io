a6d6b06d7fb89e0e3c1caf04cd10f2e4862a4117
Papik Tamas <trapacska@gmail.com>
2019-05-17 14:58:40 +0200

    handle multiple xmls and parenting of testsuites

--- test/converters/junitxml/junitxml.go
+++ test/converters/junitxml/junitxml.go
@@ -11,32 +11,51 @@ import (
 
 // Converter holds data of the converter
 type Converter struct {
-	files       []string
-	xmlFilePath string
+	files []string
 }
 
 // Detect return true if the test results a Juni4 XML file
 func (h *Converter) Detect(files []string) bool {
-	h.files = files
 	for _, file := range h.files {
 		if strings.HasSuffix(file, ".xml") {
-			h.xmlFilePath = file
-			return true
+			h.files = append(h.files, file)
 		}
 	}
-	return false
+	return len(h.files) > 0
+}
+
+func parseParseTestSuites(filePath string) ([]junit.TestSuite, error) {
+	data, err := fileutil.ReadBytesFromFile(filePath)
+	if err != nil {
+		return nil, err
+	}
+
+	var testSuites junit.XML
+	testSuitesError := xml.Unmarshal(data, &testSuites)
+	if testSuitesError == nil {
+		return testSuites.TestSuites, nil
+	}
+
+	var testSuite junit.TestSuite
+	if err := xml.Unmarshal(data, &testSuite); err != nil {
+		return nil, errors.Wrap(errors.Wrap(err, string(data)), testSuitesError.Error())
+	}
+
+	return []junit.TestSuite{testSuite}, nil
 }
 
 // XML returns the xml content bytes
 func (h *Converter) XML() (junit.XML, error) {
-	data, err := fileutil.ReadBytesFromFile(h.xmlFilePath)
-	if err != nil {
-		return junit.XML{}, err
+	var xmlContent junit.XML
+
+	for _, file := range h.files {
+		testSuites, err := parseParseTestSuites(file)
+		if err != nil {
+			return junit.XML{}, err
+		}
+
+		xmlContent.TestSuites = append(xmlContent.TestSuites, testSuites...)
 	}
 
-	var xmlContent junit.XML
-	if err := xml.Unmarshal(data, &xmlContent); err != nil {
-		return junit.XML{}, errors.Wrap(err, string(data))
-	}
 	return xmlContent, nil
 }
