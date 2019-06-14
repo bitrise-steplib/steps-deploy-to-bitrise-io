8d40bbc224d9e140d0a6791b9128bc9464046b15
Papik Tamas <trapacska@gmail.com>
2019-06-14 15:34:35 +0200

    added file tag back

--- uploaders/aabuploader.go
+++ uploaders/aabuploader.go
@@ -90,7 +90,7 @@ func DeployAAB(pth, buildURL, token, notifyUserGroups, notifyEmails, isEnablePub
 
 	// ---
 
-	uploadURL, artifactID, err := createArtifact(buildURL, token, pth, "")
+	uploadURL, artifactID, err := createArtifact(buildURL, token, pth, "file")
 	if err != nil {
 		return "", fmt.Errorf("failed to create apk artifact, error: %s", err)
 	}
