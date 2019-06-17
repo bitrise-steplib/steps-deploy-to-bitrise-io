20dfd67563e043627f4631bc5efab67d8911d689
Papik Tamas <trapacska@gmail.com>
2019-06-17 11:52:47 +0200

    added signing

--- uploaders/aabuploader.go
+++ uploaders/aabuploader.go
@@ -3,116 +3,81 @@ package uploaders
 import (
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"os"
 	"path/filepath"
-	"strings"
 
+	"github.com/bitrise-io/go-utils/command"
 	"github.com/bitrise-io/go-utils/pathutil"
 
 	"github.com/bitrise-io/go-utils/log"
 	"github.com/bitrise-steplib/steps-deploy-to-bitrise-io/bundletool"
 )
 
-func getAABInfo(toolOutput string) (ApkInfo, error) {
-	appName := filterAppLable(toolOutput)
-	packageName, versionCode, versionName := filterPackageInfos(toolOutput)
-	minSDKVersion := filterMinSDKVersion(toolOutput)
-
-	packageContent := ""
-	for _, line := range strings.Split(toolOutput, "\n") {
-		if strings.HasPrefix(line, "package:") {
-			packageContent = line
-		}
-	}
-
-	return ApkInfo{
-		AppName:           appName,
-		PackageName:       packageName,
-		VersionCode:       versionCode,
-		VersionName:       versionName,
-		MinSDKVersion:     minSDKVersion,
-		RawPackageContent: packageContent,
-	}, nil
-}
-
 // DeployAAB ...
 func DeployAAB(pth, buildURL, token, notifyUserGroups, notifyEmails, isEnablePublicPage string) (string, error) {
 	log.Printf("analyzing aab")
+
+	tmpPth, err := pathutil.NormalizedOSTempDirPath("aab-bundle")
+	if err != nil {
+		return "", err
+	}
 
 	r, err := bundletool.NewRunner()
 	if err != nil {
 		return "", err
 	}
 
-	tmpPth, err := pathutil.NormalizedOSTempDirPath("apks")
+	// create debug keystore for signing
+	keystorePath := filepath.Join(tmpPth, "debug.keystore")
+	if err := command.New("keytool", "-genkey", "-v", "-keystore", keystorePath, "-storepass", "android", "-alias", "androiddebugkey", "-keypass", "android", "-keyalg", "RSA", "-keysize", "2048", "-validity", "10000", "-dname", "C=US, O=Android, CN=Android Debug").Run(); err != nil {
+		return "", err
+	}
+
+	// generate `tmpDir/universal.apks` from aab file
+	apksPth := filepath.Join(tmpPth, "universal.apks")
+	_, err = r.Execute("build-apks", "--mode=universal", "--bundle", pth, "--output", apksPth, "--ks", keystorePath, "--ks-pass", "pass:android", "--ks-key-alias", "androiddebugkey", "--key-pass", "pass:android")
 	if err != nil {
 		return "", err
 	}
 
-	apksPth := filepath.Join(tmpPth, "apks.apks")
-
-	o, err := r.Execute("build-apks", "--mode=universal", "--bundle", pth, "--output", apksPth)
-	if err != nil {
-		return "", err
-	}
-	fmt.Println(o)
-
-	const spec = `{
-		"sdkVersion": 100,
-		"screenDensity": 560,
-		"supportedAbis": ["arm64-v8a", "armeabi-v7a", "armeabi"],
-		"supportedLocales": ["en-US"]
-	}`
-
-	specPath := filepath.Join(tmpPth, "spec.json")
-	if err := ioutil.WriteFile(specPath, []byte(spec), 0777); err != nil {
-		return "", err
-	}
-	//extract-apks --apks /tmp/lol.apks --output-dir /tmp/here --device-spec /tmp/spec.json
-	o, err = r.Execute("extract-apks", "--apks", apksPth, "--output-dir", tmpPth, "--device-spec", specPath)
-	if err != nil {
+	// unzip `tmpDir/universal.apks` to tmpPth to have `tmpDir/universal.apk`
+	if err := command.New("unzip", "-v", apksPth, "-d", tmpPth).Run(); err != nil {
 		return "", err
 	}
 
-	fmt.Println(o)
-
+	// rename `tmpDir/universal.apk` to `tmpDir/aab-name.aab.universal.apk`
 	universalAPKPath := filepath.Join(tmpPth, "universal.apk")
 	renamedUniversalAPKPath := filepath.Join(tmpPth, filepath.Base(pth)+".universal.apk")
 	if err := os.Rename(universalAPKPath, renamedUniversalAPKPath); err != nil {
 		return "", err
 	}
 
+	// get aab manifest dump
 	out, err := r.Execute("dump", "manifest", "--bundle", pth)
 	if err != nil {
 		return "", err
 	}
 
-	aabInfo, err := getAABInfo(out)
-	if err != nil {
-		return "", err
-	}
+	packageName, versionCode, versionName := filterPackageInfos(out)
 
 	appInfo := map[string]interface{}{
-		"app_name":        aabInfo.AppName,
-		"package_name":    aabInfo.PackageName,
-		"version_code":    aabInfo.VersionCode,
-		"version_name":    aabInfo.VersionName,
-		"min_sdk_version": aabInfo.MinSDKVersion,
+		"package_name": packageName,
+		"version_code": versionCode,
+		"version_name": versionName,
 	}
 
 	log.Printf("  aab infos: %v", appInfo)
 
-	if aabInfo.PackageName == "" {
-		log.Warnf("Package name is undefined, AndroidManifest.xml package content:\n%s", aabInfo.RawPackageContent)
+	if packageName == "" {
+		log.Warnf("Package name is undefined, AndroidManifest.xml package content:\n%s", out)
 	}
 
-	if aabInfo.VersionCode == "" {
-		log.Warnf("Version code is undefined, AndroidManifest.xml package content:\n%s", aabInfo.RawPackageContent)
+	if versionCode == "" {
+		log.Warnf("Version code is undefined, AndroidManifest.xml package content:\n%s", out)
 	}
 
-	if aabInfo.VersionName == "" {
-		log.Warnf("Version name is undefined, AndroidManifest.xml package content:\n%s", aabInfo.RawPackageContent)
+	if versionName == "" {
+		log.Warnf("Version name is undefined, AndroidManifest.xml package content:\n%s", out)
 	}
 
 	// ---
