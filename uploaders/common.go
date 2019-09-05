91259e4eab4da29a6e3dba05eff9a67329e191e7
Laszlo Pusok <laszlo.pusok@bitrise.io>
2019-09-05 14:30:20 +0200

    Test timeout

--- uploaders/common.go
+++ uploaders/common.go
@@ -111,7 +111,7 @@ func uploadArtifact(uploadURL, artifactPth, contentType string) error {
 	log.Printf("uploading artifact")
 
 	netClient := &http.Client{
-		Timeout: time.Second * 3,
+		Timeout: time.Second * 1,
 	}
 
 	return retry.Times(3).Wait(5).Try(func(attempt uint) error {
