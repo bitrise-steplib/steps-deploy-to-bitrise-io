17251c3a8e37b9f634e723d3d55f94e5eeb38936
Papik Tamas <trapacska@gmail.com>
2019-09-23 15:10:01 +0200

    added omitempty

--- test/junit/xml.go
+++ test/junit/xml.go
@@ -34,5 +34,5 @@ type TestCase struct {
 type Error struct {
 	XMLName xml.Name `xml:"error,omitempty"`
 	Message string   `xml:"message,attr,omitempty"`
-	Value   string   `xml:",chardata"`
+	Value   string   `xml:",chardata,omitempty"`
 }
