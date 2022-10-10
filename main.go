01c04e9e024f8c3ddd86037c1c6092dc43b2dfdb
vikas <vikas.shah@bitrise.io>
2022-10-10 09:25:41 -0500

    Removed zip modifier.

--- main.go
+++ main.go
@@ -303,10 +303,8 @@ func compressAsTar(sourceDirPath, destinationTarPath string, isContentOnly bool)
 
 	// -c - create a new archive
 	// -f - the next argument is the name of the output archive
-	// -z - filter the archive through gzip
-	// -C - this changes the directory before adding the following files
 	// Note: recursive behavior is default in tar
-	cmd := command.New("/usr/bin/tar", "cfz", destinationTarPath, sourceDirPath)
+	cmd := command.New("/usr/bin/tar", "cf", destinationTarPath, sourceDirPath)
 
 	if out, err := cmd.RunAndReturnTrimmedCombinedOutput(); err != nil {
 		err = fmt.Errorf("command: (%s) failed, output: %s, error: %s", cmd.PrintableCommandArgs(), out, err)
