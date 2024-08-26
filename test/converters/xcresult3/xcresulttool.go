133584195a5da86f4f4f9ed8f70e5f6d327dc70e
lpusok <7979773+lpusok@users.noreply.github.com>
2024-08-26 15:28:08 +0200

    Use legacy flag for xcresulttool, needed for Xcode 16 support. (#209)
    * Use legacy flag for xcresulttool, needed for Xcode 16 support.
    
    * Remove artifacts

--- test/converters/xcresult3/xcresulttool.go
+++ test/converters/xcresult3/xcresulttool.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"os/exec"
+	"regexp"
+	"strconv"
 
 	"github.com/bitrise-io/go-utils/command"
 	"github.com/bitrise-io/go-utils/errorutil"
@@ -16,11 +18,45 @@ func isXcresulttoolAvailable() bool {
 	return command.New("xcrun", "--find", "xcresulttool").Run() == nil
 }
 
+func isLegacyFlagNeededForXcresulttoolVersion() (bool, error) {
+	args := []string{"xcresulttool", "version"}
+	cmd := command.New("xcrun", args...)
+	out, err := cmd.RunAndReturnTrimmedCombinedOutput()
+	if err != nil {
+		if errorutil.IsExitStatusError(err) {
+			return true, fmt.Errorf("%s failed: %s", cmd.PrintableCommandArgs(), out)
+		}
+		return true, fmt.Errorf("%s failed: %s", cmd.PrintableCommandArgs(), err)
+	}
+	// xcresulttool version 23025, format version 3.53 (current)
+	versionRegexp := regexp.MustCompile("xcresulttool version ([0-9]+)")
+
+	matches := versionRegexp.FindStringSubmatch(out)
+	if len(matches) < 2 {
+		return true, fmt.Errorf("no version matches found in output: %s", out)
+	}
+
+	version, err := strconv.Atoi(matches[1])
+	if err != nil {
+		return true, fmt.Errorf("failed to convert version: %s", matches[1])
+	}
+
+	return version >= 23_021, nil // Xcode 16 beta3 has version 23021
+}
+
 // xcresulttoolGet performs xcrun xcresulttool get with --id flag defined if id provided and marshals the output into v.
 func xcresulttoolGet(xcresultPth, id string, v interface{}) error {
 	args := []string{"xcresulttool", "get", "--format", "json", "--path", xcresultPth}
 	if id != "" {
 		args = append(args, "--id", id)
+	}
+
+	isLegacyFlag, err := isLegacyFlagNeededForXcresulttoolVersion()
+	if err != nil {
+		return err
+	}
+	if isLegacyFlag {
+		args = append(args, "--legacy")
 	}
 
 	cmd := command.New("xcrun", args...)
@@ -40,6 +76,14 @@ func xcresulttoolGet(xcresultPth, id string, v interface{}) error {
 // xcresulttoolExport exports a file with the given id at the given output path.
 func xcresulttoolExport(xcresultPth, id, outputPth string) error {
 	args := []string{"xcresulttool", "export", "--path", xcresultPth, "--id", id, "--output-path", outputPth, "--type", "file"}
+	isLegacyFlag, err := isLegacyFlagNeededForXcresulttoolVersion()
+	if err != nil {
+		return err
+	}
+	if isLegacyFlag {
+		args = append(args, "--legacy")
+	}
+
 	cmd := command.New("xcrun", args...)
 	out, err := cmd.RunAndReturnTrimmedCombinedOutput()
 	if err != nil {
