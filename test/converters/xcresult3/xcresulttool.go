389e9d7e71614b80ac13d5b6e1e1879738d21c8c
Szabolcs Toth <54896607+tothszabi@users.noreply.github.com>
2025-03-19 16:33:56 +0100

    Add support for the new extraction method

--- test/converters/xcresult3/xcresulttool.go
+++ test/converters/xcresult3/xcresulttool.go
@@ -18,7 +18,7 @@ func isXcresulttoolAvailable() bool {
 	return command.New("xcrun", "--find", "xcresulttool").Run() == nil
 }
 
-func isLegacyFlagNeededForXcresulttoolVersion() (bool, error) {
+func supportsNewExtractionMethods() (bool, error) {
 	args := []string{"xcresulttool", "version"}
 	cmd := command.New("xcrun", args...)
 	out, err := cmd.RunAndReturnTrimmedCombinedOutput()
@@ -42,21 +42,31 @@ func isLegacyFlagNeededForXcresulttoolVersion() (bool, error) {
 	}
 
 	return version >= 23_021, nil // Xcode 16 beta3 has version 23021
+
+	//fmt.Println("xcresulttool version:", version)
+	//
+	//return false, nil
 }
 
 // xcresulttoolGet performs xcrun xcresulttool get with --id flag defined if id provided and marshals the output into v.
 func xcresulttoolGet(xcresultPth, id string, v interface{}) error {
-	args := []string{"xcresulttool", "get", "--format", "json", "--path", xcresultPth}
-	if id != "" {
-		args = append(args, "--id", id)
-	}
+	args := []string{"xcresulttool", "get"}
 
-	isLegacyFlag, err := isLegacyFlagNeededForXcresulttoolVersion()
+	supportsNewMethod, err := supportsNewExtractionMethods()
 	if err != nil {
 		return err
 	}
-	if isLegacyFlag {
-		args = append(args, "--legacy")
+
+	if supportsNewMethod {
+		args = append(args, "test-results", "tests")
+	} else {
+		args = append(args, "--format", "json")
+	}
+
+	args = append(args, "--path", xcresultPth)
+
+	if id != "" {
+		args = append(args, "--id", id)
 	}
 
 	cmd := command.New("xcrun", args...)
@@ -75,13 +85,24 @@ func xcresulttoolGet(xcresultPth, id string, v interface{}) error {
 
 // xcresulttoolExport exports a file with the given id at the given output path.
 func xcresulttoolExport(xcresultPth, id, outputPth string) error {
-	args := []string{"xcresulttool", "export", "--path", xcresultPth, "--id", id, "--output-path", outputPth, "--type", "file"}
-	isLegacyFlag, err := isLegacyFlagNeededForXcresulttoolVersion()
+	args := []string{"xcresulttool", "export"}
+
+	supportsNewMethod, err := supportsNewExtractionMethods()
 	if err != nil {
 		return err
 	}
-	if isLegacyFlag {
-		args = append(args, "--legacy")
+
+	if supportsNewMethod {
+		args = append(args, "attachments")
+	} else {
+		args = append(args, "--type", "file")
+	}
+
+	args = append(args, "--path", xcresultPth)
+	args = append(args, "--output-path", outputPth)
+
+	if id != "" {
+		args = append(args, "--id", id)
 	}
 
 	cmd := command.New("xcrun", args...)
