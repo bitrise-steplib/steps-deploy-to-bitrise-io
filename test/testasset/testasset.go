3216f090accf89292e8546fae5bd6d2cdf518f43
Andras Eszes <andras.eszes@bitrise.io>
2025-11-18 15:30:09 +0100

    fix: review comment

--- test/testasset/testasset.go
+++ test/testasset/testasset.go
@@ -7,16 +7,18 @@ import (
 )
 
 var AssetTypes = []string{".jpg", ".jpeg", ".png", ".txt", ".log"}
-
-// NOTE: These video types are also supported on the UI
-var AssetTypesWithVideo = append(AssetTypes, ".mp4", ".webm", ".ogg")
+var VideoTypes = []string{".mp4", ".webm", ".ogg"} // These video types are also supported on the UI
 
 func IsSupportedAssetType(fileName string) bool {
 	ext := filepath.Ext(fileName)
 
-	if os.Getenv("ENABLE_TEST_VIDEO_UPLOAD") == "true" {
-		return slices.Contains(AssetTypesWithVideo, ext)
+	if slices.Contains(AssetTypes, ext) {
+		return true
 	}
 
-	return slices.Contains(AssetTypes, ext)
+	if os.Getenv("ENABLE_TEST_VIDEO_UPLOAD") == "true" {
+		return slices.Contains(VideoTypes, ext)
+	}
+
+	return false
 }
