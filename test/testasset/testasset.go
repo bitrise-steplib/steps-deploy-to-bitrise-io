19537e85da2b9e64efff41b72219ac138447d9c3
Andras Eszes <andras.eszes@bitrise.io>
2025-11-19 08:53:59 +0100

    [CI-5535] Enable video upload for tests (#249)
    * [CI-5535] Enable video upload for tests
    
    * fix: review comment
    
    * Trigger CI

--- test/testasset/testasset.go
+++ test/testasset/testasset.go
@@ -1,13 +1,24 @@
 package testasset
 
 import (
+	"os"
 	"path/filepath"
 	"slices"
 )
 
 var AssetTypes = []string{".jpg", ".jpeg", ".png", ".txt", ".log"}
+var VideoTypes = []string{".mp4", ".webm", ".ogg"} // These video types are also supported on the UI
 
 func IsSupportedAssetType(fileName string) bool {
 	ext := filepath.Ext(fileName)
-	return slices.Contains(AssetTypes, ext)
+
+	if slices.Contains(AssetTypes, ext) {
+		return true
+	}
+
+	if os.Getenv("ENABLE_TEST_VIDEO_UPLOAD") == "true" {
+		return slices.Contains(VideoTypes, ext)
+	}
+
+	return false
 }
