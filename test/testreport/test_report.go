b91a376b28a16d5e041dffa1ff23260b72bd7996
Zoltan Szabo <zoltan.szabo@bitrise.io>
2025-12-15 19:55:18 +0100

    fix: extend test_report models

--- test/testreport/test_report.go
+++ test/testreport/test_report.go
@@ -15,6 +15,7 @@ type TestSuite struct {
 	Name       string      `xml:"name,attr"`
 	Tests      int         `xml:"tests,attr"`
 	Failures   int         `xml:"failures,attr"`
+	Errors     int         `xml:"errors,attr"`
 	Skipped    int         `xml:"skipped,attr"`
 	Time       float64     `xml:"time,attr"`
 	TestCases  []TestCase  `xml:"testcase"`
@@ -25,15 +26,21 @@ type TestCase struct {
 	XMLName xml.Name `xml:"testcase"`
 	// ConfigurationHash is used to distinguish the same test case runs,
 	// performed with different build configurations (e.g., Debug vs. Release) or different devices/simulators
-	ConfigurationHash string  `xml:"configuration-hash,attr"`
-	Name              string  `xml:"name,attr"`
-	ClassName         string  `xml:"classname,attr"`
-	Time              float64 `xml:"time,attr"`
-	// TODO: Currently a JUnit report's TestCase.Error and TestCase.SystemErr is merged into TestCase.Failure.Value field,
-	// this way test execution errors are not distinguished from test failures.
-	Failure    *Failure    `xml:"failure,omitempty"`
-	Skipped    *Skipped    `xml:"skipped,omitempty"`
-	Properties *Properties `xml:"properties,omitempty"`
+	ConfigurationHash string      `xml:"configuration-hash,attr"`
+	Name              string      `xml:"name,attr"`
+	ClassName         string      `xml:"classname,attr"`
+	Time              float64     `xml:"time,attr"`
+	Error             *Error      `xml:"error,omitempty"`
+	Failure           *Failure    `xml:"failure,omitempty"`
+	Skipped           *Skipped    `xml:"skipped,omitempty"`
+	Properties        *Properties `xml:"properties,omitempty"`
+	SystemOut         *SystemOut  `xml:"system-out,omitempty"`
+	SystemErr         *SystemErr  `xml:"system-err,omitempty"`
+}
+
+type Error struct {
+	XMLName xml.Name `xml:"error,omitempty"`
+	Value   string   `xml:",chardata"`
 }
 
 type Failure struct {
@@ -43,6 +50,7 @@ type Failure struct {
 
 type Skipped struct {
 	XMLName xml.Name `xml:"skipped,omitempty"`
+	Value   string   `xml:",chardata"`
 }
 
 type Property struct {
@@ -54,4 +62,14 @@ type Property struct {
 type Properties struct {
 	XMLName  xml.Name   `xml:"properties"`
 	Property []Property `xml:"property"`
+}
+
+type SystemOut struct {
+	XMLName xml.Name `xml:"system-out,omitempty"`
+	Value   string   `xml:",chardata"`
+}
+
+type SystemErr struct {
+	XMLName xml.Name `xml:"system-err,omitempty"`
+	Value   string   `xml:",chardata"`
 }
