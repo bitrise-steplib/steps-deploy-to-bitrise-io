fd4edc85c202ea39ecca7a3f9012e0d1ff750cb1
Andras Eszes <andras.eszes@bitrise.io>
2026-02-10 10:42:22 +0100

    fix: pr comment

--- test/testasset/testasset.go
+++ test/testasset/testasset.go
@@ -10,10 +10,5 @@ var AssetTypes = []string{".jpg", ".jpeg", ".png", ".txt", ".log", ".mp4", ".web
 
 func IsSupportedAssetType(fileName string) bool {
 	ext := filepath.Ext(fileName)
-
-	if slices.Contains(AssetTypes, strings.ToLower(ext)) {
-		return true
-	}
-
-	return false
+	return slices.Contains(AssetTypes, strings.ToLower(ext))
 }
